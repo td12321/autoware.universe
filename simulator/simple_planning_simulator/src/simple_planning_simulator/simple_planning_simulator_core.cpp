db48e1c6baadcb66ae14ca0366cb0c4b82ea0ae0
td12321 <taichi.hirano@tier4.jp>
2022-01-19 14:39:43 +0900

    The operation check using only the front input has been completed on the 4ws model for the simulator.

--- simulator/simple_planning_simulator/src/simple_planning_simulator/simple_planning_simulator_core.cpp
+++ simulator/simple_planning_simulator/src/simple_planning_simulator/simple_planning_simulator_core.cpp
@@ -357,6 +357,9 @@ void SimplePlanningSimulator::set_input(const float steer, const float vel, cons
   {
     input << acc, steer, 0;
   }
+  /* for 4ws debug*/
+  input << acc, steer, 0;
+  /**/
   vehicle_model_ptr_->setInput(input);
 }
 
@@ -454,6 +457,11 @@ void SimplePlanningSimulator::set_initial_state(const Pose & pose, const Twist &
   {
     state << x, y, yaw, vx, steer, 0, accx;
   }
+
+  /*for 4ws debug*/
+    state << x, y, yaw, 0, 0, 0, 0;
+	/**/
+
   vehicle_model_ptr_->setState(state);
 
   is_initialized_ = true;
