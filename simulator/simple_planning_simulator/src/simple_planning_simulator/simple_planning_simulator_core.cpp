97d0ff66ed0453414f0e4e5b622848f1ccfc685e
td12321 <taichi.hirano@tier4.jp>
2022-02-02 10:39:52 +0900

    Corresponds to the change of control_cmd (in progress)

--- simulator/simple_planning_simulator/src/simple_planning_simulator/simple_planning_simulator_core.cpp
+++ simulator/simple_planning_simulator/src/simple_planning_simulator/simple_planning_simulator_core.cpp
@@ -320,6 +320,8 @@ void SimplePlanningSimulator::on_ackermann_cmd(
   current_ackermann_cmd_ptr_ = msg;
   set_input(
     msg->lateral.steering_tire_angle, msg->longitudinal.speed, msg->longitudinal.acceleration);
+    msg->lateral.front_steering_tire_angle, msg->longitudinal.speed,
+    msg->longitudinal.acceleration);
 }
 
 void SimplePlanningSimulator::set_input(const float steer, const float vel, const float accel)
